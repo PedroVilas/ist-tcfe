9b9f52ca035295bf5affca2383bba62584197cda
PedroVilas <pedro.vilas@tecnico.ulisboa.pt>
2021-06-09 00:09:31 +0100

    final

---
+++ t5/data_gen.m
@@ -0,0 +1,72 @@
+close all
+clear all
+
+%variables for opamp bandpass filter-----------------------------------------
+
+diary data_octave.txt
+diary on
+Vcc = 5
+Vee = -5
+Vin_amp_ac = 1
+Vin_param2 = 0.01
+Vinf = 1000
+
+R1 = 10000
+R2 = 10000
+R3 = 100e3
+R4 = 1000
+C1 = 220e-9
+C2 = 220e-9
+
+
+
+diary off
+
+%variables for ngspice-----------------------------------------
+
+diary data_t5.txt
+diary on
+
+printf("Vcc vcc 0 %d\n", Vcc)
+printf("Vee vee 0 %d\n", Vee)
+printf("Vin in 0 0 ac %d sin(0 %d %d)\n", Vin_amp_ac, Vin_param2, Vinf)
+
+printf("C1 in n_inv %d\n", C1)
+printf("R1 n_inv 0 %d\n", R1)
+printf("R3 amp inv_in %d\n", R3)
+printf("R4 inv_in 0 %d\n", R4)
+
+printf("X1 n_inv inv_in vcc vee out uA741\n")
+
+printf("R2 amp out %d\n", R2)
+printf("C2 out 0 %d\n", C2)
+
+
+diary off
+
+
+diary data_t5_inc.txt
+diary on
+
+printf("Vcc vcc 0 %d\n", Vcc)
+printf("Vee vee 0 %d\n", Vee)
+%printf("Vin in 0 0 ac %d sin(0 %d %d)\n", Vin_amp_ac, Vin_param2, Vinf)
+printf("Vin in 0 0\n")
+printf("C1 in n_inv %d\n", C1)
+printf("R1 n_inv 0 %d\n", R1)
+printf("R3 amp inv_in %d\n", R3)
+printf("R4 inv_in 0 %d\n", R4)
+
+printf("X1 n_inv inv_in vcc vee out uA741\n")
+
+printf("R2 amp out %d\n", R2)
+printf("C2 out 0 %d\n", C2)
+
+printf("Vo out 0 0 ac 1 sin(0 1 %d)\n", Vinf)
+
+diary off
+
+
+
+
+
---
+++ t5/eval_sim.m
@@ -0,0 +1,91 @@
+close all
+clear all
+
+%data generation ------------------------------------------
+
+data = fopen('data_octave.txt','r');
+DATA = fscanf(data,'%*s = %f');
+fclose(data);
+
+
+R1 = DATA(6)
+R2 = DATA(7)
+R3 = DATA(8)
+R4 = DATA(9)
+C1 = DATA(10)
+C2 = DATA(11)
+
+cost = (R1+R2+R3+R4)/1000 + (C1+C2)*1000000 +1.3323e4
+
+%data from octave ------------------------------------------
+data_oct = fopen('./mat/results_oct.txt','r');
+DATAOCT = fscanf(data_oct,'%*s = %f');
+fclose(data_oct);
+
+lco_oct = DATAOCT(1)/2/pi
+uco_oct = DATAOCT(2)/2/pi
+fo_oct =DATAOCT(4)
+gain_oct = DATAOCT(5)
+gain_db_oct = DATAOCT(6)
+Z_in_oct = DATAOCT(7)
+Z_out_oct = DATAOCT(8) 
+
+gain_deviation_oct = abs(100-gain_oct)
+frequency_deviation_oct = abs(fo_oct-1000)
+
+MERIT_oct = 1/cost/(gain_deviation_oct + frequency_deviation_oct+1e-6);
+
+%data from ngspice ---------------------------------------------------
+
+data_sim = fopen('./sim/result_sim.txt','r');
+DATASIM = fscanf(data_sim,'%*s = %f');
+fclose(data_sim);
+
+uco_sim = DATASIM(1)
+lco_sim = DATASIM(2)
+fo_sim =DATASIM(3)
+gain_sim = DATASIM(5)
+gain_db_sim = DATASIM(6)
+Z_in_sim = DATASIM(7)
+Z_out_sim = DATASIM(8) 
+
+gain_deviation_sim = abs(100-gain_sim)
+frequency_deviation_sim = abs(fo_sim-1000)
+
+MERIT_sim = 1/cost/(gain_deviation_sim + frequency_deviation_sim+1e-6);
+
+
+diary merit.tex
+diary on
+
+printf('$Zi_{total}$ & %d & %d & Ohm\n', Z_in_sim, Z_in_oct);
+printf('$Zo_{total}$ & %d & %d & Ohm\n', Z_out_sim, Z_out_oct);
+
+printf('uco & %.3f & %.3f & Hz\n', uco_sim, uco_oct);
+printf('lco & %.3f & %.3f & Hz\n', lco_sim, lco_oct);
+printf('fo & %.3f & %.3f & Hz\n', fo_sim, fo_oct);
+printf('$f_{deviation}$ & %.3f & %.3f & Hz\n', frequency_deviation_sim , frequency_deviation_oct);
+
+
+printf('$Gain_{total}$ & %.3f & %.3f & -\n', gain_sim, gain_oct);
+printf('$Gain_{deviation}$ & %.3f & %.3f & -\n', gain_deviation_sim , gain_deviation_oct);
+
+printf('Cost & %d & %d & MU\n', cost, cost);
+
+printf('MERIT & %.8f & %.8f & -\n', MERIT_sim, MERIT_oct);
+diary off
+
+
+
+diary values.tex
+diary on
+
+printf('R1 & %d & Ohm\n', R1);
+printf('R2 & %d & Ohm\n', R2);
+printf('R3 & %d & Ohm\n', R3);
+printf('R4 & %d & Ohm\n', R4);
+printf('C1 & %d & Farad\n', C1);
+printf('C2 & %d & Farad\n', C2);
+
+diary off
+
